f9127c042039717a487db20d485f919732b78163
Obscurity96 <159748221+Obscurity96@users.noreply.github.com>
2024-03-08 13:17:08 -0700

    Actividades Visual code

---
+++ ActividadAyB.py
@@ -0,0 +1,12 @@
+# Actividad solicitar a y b
+
+A = float(input("Ingrese el número A: "))
+B = float(input("Ingrese el número B: "))
+
+# Verificar si B es mayor que A
+if B > A:
+    
+    # Mostrar solo si B es mayor que A
+    print("B es mayor que A.")
+    print("A:", A)
+    print("B:", B)
