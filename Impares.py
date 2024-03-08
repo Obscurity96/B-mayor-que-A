4015a47bdc57767bbeef979a736183d4b412544c
Obscurity96 <159748221+Obscurity96@users.noreply.github.com>
2024-03-08 14:04:41 -0700

    Add files via upload

---
+++ Impares.py
@@ -0,0 +1,8 @@
+# Actividad numeros impar
+
+numero = int(input("Ingrese un número: "))
+# Verificar si es numero par o impar
+if numero % 2 == 0:
+    print("El número ingresado es par.")
+else:
+    print("El número ingresado es impar.")
