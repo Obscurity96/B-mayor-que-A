af8b6096ba3ea93fa3f48449c5ab86cf89ec2185
Obscurity96 <159748221+Obscurity96@users.noreply.github.com>
2024-03-08 21:18:12 -0700

    Ladas.py
    Actividad Ladas

---
+++ Ladas.py
@@ -0,0 +1,25 @@
+# Actividad de ladas
+
+lada = input("Ingrese lada: ")
+if lada == "55":
+    print("CDMDX")
+elif lada == "81":
+    print("Monterrey")
+elif lada == "656":
+    print("Ciudad juarez")
+elif lada == "442":
+    print("Queretaro")
+elif lada == "664":
+    print("Tijuana")
+elif lada == "33":
+    print("Jalisco")
+elif lada == "447":
+    print("León")
+elif lada == "612":
+    print("La paz")
+elif lada == "998":
+    print("Cancún")
+elif lada == "228":
+    print("Xalapa")
+else:
+    print("Lada no conocida")
